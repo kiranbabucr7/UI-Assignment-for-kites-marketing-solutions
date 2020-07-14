1f4f4a30f56d005c2a7d76ca435e89dd0eaf077e
kiranbabucr7 <kiranbabu716@gmail.com>
2020-07-14 20:24:29 +0530

    Added Footer section

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
 	// jQuery code
-
+	function myMap() {
+		var mapProp= {
+		  center:new google.maps.LatLng(51.508742,-0.120850),
+		  zoom:5,
+		};
+		var map = new google.maps.Map(document.getElementById("map-container"),mapProp);
+	}
 	//////////////////////// Prevent closing from click inside dropdown
     $(document).on('click', '.dropdown-menu', function (e) {
       e.stopPropagation();
